e8ce204169f0a036419785adb9db264ddea2cadc
viktormark <viktormarkovetskyi@gmail.com>
2023-08-24 20:49:55 +0300

    fix

--- test_ui.py
+++ test_ui.py
@@ -29,7 +29,9 @@ def driver():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
+
+
 
     driver.implicitly_wait(10)
     yield driver
