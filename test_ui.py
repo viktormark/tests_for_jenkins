e8bd3b19fd3c82f5fab63757138216de7a338ebf
viktormark <viktormarkovetskyi@gmail.com>
2023-08-24 21:24:48 +0300

    fix

--- test_ui.py
+++ test_ui.py
@@ -32,9 +32,6 @@ def driver():
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--verbose')
     driver = webdriver.Chrome(options=chrome_options)
-
-
-
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
