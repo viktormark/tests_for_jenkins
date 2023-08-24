50fdd0b850723ee27c8cd2645364a74b8ebdbe27
viktormark <viktormarkovetskyi@gmail.com>
2023-08-24 20:32:56 +0300

    fix

--- test_ui.py
+++ test_ui.py
@@ -6,20 +6,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
+# @pytest.fixture
+# def driver():
+#     chrome_options = webdriver.ChromeOptions()
+#     chrome_options.binary_location = '/usr/local/bin/chrome'
+#     chrome_options.add_argument("--headless")
+#     chrome_options.add_argument("--disable-dev-shm-usage")
+#     chrome_options.add_argument("--no-sandbox")
+#
+#     service = ChromeService(executable_path='/usr/local/bin/chromedriver')
+#     driver = webdriver.Chrome(service=service, options=chrome_options)
+#
+#     driver.implicitly_wait(10)
+#     yield driver
+#     driver.quit()
+
+
 @pytest.fixture
 def driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = '/usr/local/bin/chrome'
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    # options = Options()
-    # options.binary_location = '/usr/bin/google-chrome'
-    #options.add_argument("--headless")
-    # options.add_argument("--disable-gpu")
-    # options.add_argument("--no-sandbox")
-    #
-    service = ChromeService(executable_path='/usr/local/bin/chromedriver')
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    options = Options()
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=options)
 
     driver.implicitly_wait(10)
     yield driver
