f0516a6d14697a948462f4b68891ceb1e0489d72
viktormark <viktormarkovetskyi@gmail.com>
2023-08-24 14:52:20 +0300

    fix

--- test_ui.py
+++ test_ui.py
@@ -7,11 +7,8 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    options = Options()
-    options.add_argument("--headless")
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
+    driver = webdriver.Chrome()
+    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
@@ -31,14 +28,3 @@ def test_add_remove_elements(driver):
     buttons = driver.find_elements(By.XPATH, "//button[text()='Delete']")
     assert len(buttons) == 1
 
-
-def test_authentication(driver):
-    username = "tomsmith"
-    password = "SuperSecretPassword!"
-    driver.get("http://the-internet.herokuapp.com/")
-    driver.find_element(By.XPATH, '//*[@id="content"]/ul/li[21]/a').click()
-    driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(username)
-    driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
-    driver.find_element(By.XPATH, '//*[@id="login"]/button').click()
-    text = driver.find_element(By.XPATH, '//*[@id="flash"]')
-    assert "You logged into a secure area!\n×" == text.text
