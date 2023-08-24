cc6eb9deb5e30916b629ffc94a1e6f4fcc19f892
viktormark <viktormarkovetskyi@gmail.com>
2023-08-24 21:00:04 +0300

    fix

--- test_ui.py
+++ test_ui.py
@@ -26,6 +26,7 @@ from selenium.webdriver.chrome.service import Service
 @pytest.fixture
 def driver():
     chrome_options = Options()
+    chrome_options.binary_location = '/usr/local/bin/chrome'
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
