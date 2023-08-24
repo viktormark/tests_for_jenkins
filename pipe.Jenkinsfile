fa2c4ef088cadf1b82a71d5316aad36391d4a429
viktormark <viktormarkovetskyi@gmail.com>
2023-08-24 19:56:41 +0300

    fix

--- pipe.Jenkinsfile
+++ pipe.Jenkinsfile
@@ -1,8 +1,5 @@
 pipeline {
     agent any
-    environment {
-        PATH = "/usr/local/bin:$PATH"
-    }
     stages {
         stage('Install Dependencies') {
             steps {
--- test_ui.py
+++ test_ui.py
@@ -8,20 +8,17 @@ from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
 def driver():
-
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = '/usr/local/bin/chrome'
     chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-
-
     # options = Options()
     # options.binary_location = '/usr/bin/google-chrome'
-    # options.add_argument("--headless")
+    #options.add_argument("--headless")
     # options.add_argument("--disable-gpu")
     # options.add_argument("--no-sandbox")
     #
-    # service = ChromeService(executable_path='/usr/local/bin/chromedriver')
-    # driver = webdriver.Chrome(service=service, options=options)
+    service = ChromeService(executable_path='/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.implicitly_wait(10)
     yield driver
