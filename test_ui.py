22ff209dc9879fc97f74fb1164f8d64e20436fe0
viktormark <viktormarkovetskyi@gmail.com>
2023-08-24 13:59:12 +0300

    fix

--- test_ui.py
+++ test_ui.py
@@ -9,7 +9,6 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     options = Options()
     options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
